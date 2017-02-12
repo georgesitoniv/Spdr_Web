30f3f81670a7d89005b862494ce52ed484dfd116
GeorgeIV <georgesitoniv@gmail.com>
2017-02-12 20:14:43 +0800

    Fixing

--- js/xion.js
+++ js/xion.js
@@ -7,7 +7,6 @@ $( document ).ready(function(){
 $('a[href*="#"]:not([href="#"])').click(function() {
     $("li").removeClass('active');
     $(this).parents('li').addClass('active');
-    
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
